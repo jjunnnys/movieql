c4a4e7ed86aee14fe21814bcdc8a94d355679602
jjunnnys <salswnss@gmail.com>
2020-12-30 12:17:21 +0900

    #0.8 Creating Queries with Arguments

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,10 @@ import { getById, people } from './db';
 const resolvers = {
     Query: {
         people: () => people,
-        person: () => getById(),
+        person: (_, { id }) => {
+            // 처음 파라미터는 현재 객체를 보냄
+            return getById(id);
+        },
     },
 };
 
